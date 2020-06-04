956a6a565ef03caaac76eeea4312780f396d9635
Wasim Almadfaa <wm.madfaa@gmail.com>
2020-06-04 10:18:04 +0200

    add api config && api mock data

--- src/configs/api.configs.ts
+++ src/configs/api.configs.ts
@@ -1,35 +1,79 @@
-export interface PrayTime {
-  date: string;
-  availablePlaces: number;
+import mockApi from './api.mock.json';
+
+function mockTimeOut(abort: AbortSignal, shouldFail: boolean = false) {
+  return new Promise((res, rej) => {
+    const wait = setTimeout(() => {
+      if (abort.aborted) {
+        rej(new Error('abort error: from mockTimeOut'));
+        clearTimeout(wait);
+      }
+      if (shouldFail) {
+        rej(new Error('fail error: from mockTimeOut'));
+        clearTimeout(wait);
+      }
+      res(true);
+      clearTimeout(wait);
+    }, Math.pow(60, 2) * 1.5);
+  });
 }
 
-export const fetchPrayTimes = async (signal: AbortSignal) => {
-  return await new Promise<PrayTime[]>((res, rej) => {
-    const timeOut = setTimeout(() => {
-      res([]);
-    }, 1500);
+export async function getFridayPraying(abort: AbortSignal) {
+  try {
+    const result = await mockTimeOut(abort);
+    if (result) {
+      return mockApi['friday-praying'].get;
+    }
+  } catch (err) {
+    throw err;
+  }
+}
 
-    signal.onabort = () => {
-      clearTimeout(timeOut);
-      rej('Aborted!');
-    };
-  });
-};
+export async function getFridayPrayingForUser(
+  abort: AbortSignal,
+  _data: {token: string},
+) {
+  try {
+    const result = await mockTimeOut(abort);
+    if (result) {
+      return mockApi['friday-praying'].post;
+    }
+  } catch (err) {
+    throw err;
+  }
+}
 
-export const sign_in = async (
-  firstName: string,
-  secundName: string,
-  phoneNumber: string,
-  signal: AbortSignal,
-) => {
-  return await new Promise((res, rej) => {
-    const timeOut = setTimeout(() => {
-      res('success!');
-    }, 1500);
+export async function createPrayer(
+  abort: AbortSignal,
+  _data: {
+    phone: string;
+    firstName: string;
+    lastName: string;
+    reservePrayingTime: Date;
+    token: string;
+  },
+) {
+  try {
+    const result = await mockTimeOut(abort);
+    if (result) {
+      return mockApi['create-prayer'].post;
+    }
+  } catch (err) {
+    throw err;
+  }
+}
 
-    signal.onabort = () => {
-      clearTimeout(timeOut);
-      rej('Aborted!');
-    };
-  });
-};
+export async function deletePrayer(
+  abort: AbortSignal,
+  _data: {
+    token: string;
+  },
+) {
+  try {
+    const result = await mockTimeOut(abort);
+    if (result) {
+      return mockApi['delete-prayer'].delete;
+    }
+  } catch (err) {
+    throw err;
+  }
+}
